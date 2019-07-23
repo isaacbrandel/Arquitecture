37645bd35e5c15a21439b5fe37bcc1400123cca6
Montoya45 <Montoya45@DESKTOP-SUON9BA>
2019-07-23 13:37:40 -0600

    Creando todo

---
+++ The Chat Of Cthulhu/Pantalla_Principal.web/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using Pantalla_Principal.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pantalla_Principal.web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            var usuario = new RegistrationModel();
+            usuario.Nickname = "Mariale";
+            usuario.Name = "Alejandra";
+            usuario.Lastname = "Fajardo";
+            usuario.Correo = "Maryalecf@gmail.com";
+            return View(usuario);
+        }
+    }
+}
---
+++ The Chat Of Cthulhu/Pantalla_Principal.web/Models/RegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pantalla_Principal.web.Models
+{
+    public class RegistrationModel
+    {
+        public string Nickname { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Correo { get; set; }
+    }
+}
